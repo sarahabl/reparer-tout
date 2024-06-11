30707ae356fd1f5efc5dd4a2f0f46956e422929b
sarahabl <sarahablonet.studio@gmail.com>
2024-06-11 15:23:42 +0200

    animation js OK et effet hover

--- script.js
+++ script.js
@@ -1,23 +1,37 @@
+// ANIMATION BANDEAU STATS AVEC COMPTEUR
+
 const stats = document.querySelectorAll('.stat-item h2');
 const increment = 1; // Valeur d'incrémentation (vous pouvez ajuster selon vos besoins)
 
 function compteur() {
-    stats.forEach(stat => {
-        const cible = parseInt(stat.innerText);
-        const étape = Math.ceil(cible / 100); // Divisez la cible par le nombre d'étapes souhaité
+stats.forEach(stat => {
+const cible = parseInt(stat.innerText);
+const étape = Math.ceil(cible / 100); // Divisez la cible par le nombre d'étapes souhaité
 
-        let valeurActuelle = 10;
-        const intervalle = setInterval(() => {
-            valeurActuelle += étape;
-            stat.innerText = valeurActuelle;
+let valeurActuelle = 10;
+const intervalle = setInterval(() => {
+valeurActuelle += étape;
+stat.innerText = valeurActuelle;
 
-            if (valeurActuelle >= cible) {
-                clearInterval(intervalle);
-                stat.innerText = cible; // Assurez-vous que la valeur finale est exacte
-            }
-        }, 10); // Réglez l'intervalle de mise à jour (en millisecondes)
-    });
+if (valeurActuelle >= cible) {
+clearInterval(intervalle);
+stat.innerText = cible; // Assurez-vous que la valeur finale est exacte
+}
+}, 10); // Réglez l'intervalle de mise à jour (en millisecondes)
+});
 }
 
 // Appelez la fonction immédiatement après le chargement de la page (sans délai)
 window.addEventListener('load', compteur);
+
+
+// GARDER LA NAV BAR FIXE
+$(window).scroll(function() {
+var scrollPosition = $(this).scrollTop();
+if (scrollPosition >= 200) {
+$('.nav').addClass('fixed');
+} else {
+$('.nav').removeClass('fixed');
+}
+});
+
